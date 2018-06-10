a3dc98ddbbd758a8bc84ed144edf08f8134d8d3c
weiyulincode <1261667742@qq.com>
2018-06-11 00:38:57 +0800

    添加了 User  模块

---
+++ user/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+from user.forms import RegisterForm
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+        else:
+            return render(request, "register.html", {})
+
+
+def login(request):
+    return render(request, "login.html", {})
+
+
+def user_info(request):
+    return render(request, "user_info.html", {})
+
+
+def logout(request):
+    return render(request, "logout.html", {})
